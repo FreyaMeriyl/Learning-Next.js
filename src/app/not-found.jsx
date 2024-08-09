203b02cca4271701ff692f1539321f46b0709264
FreyaMeriyl <freyameriyal8@gmail.com>
2024-08-09 16:44:35 +0530

    Task 2 completed

---
+++ src/app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="min-h-screen bg-orange-100 justify-center items-center pt-20">
+        <h1 className="font-bold mb-4 text-gray-700 text-center">404 - Page Not Found</h1>
+        <p className="mb-4 text-gray-700 text-center">The page you are looking for does not exist.</p>
+      </div>
+    );
+  }
